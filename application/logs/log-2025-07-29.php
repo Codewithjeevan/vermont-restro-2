b54ea42b8784b338c90130a497eb48ae13457536
codewithjeevan <jeevansahu027@gmail.com>
2026-07-21 22:27:32 +0530

    Initial commit: iRestora PLUS restaurant management system
    Excludes live database, Stripe, and PayPal credentials via .gitignore.

---
+++ application/logs/log-2025-07-29.php
@@ -0,0 +1,89 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2025-07-29 17:36:03 --> Config Class Initialized
+INFO - 2025-07-29 17:36:03 --> Hooks Class Initialized
+DEBUG - 2025-07-29 17:36:03 --> UTF-8 Support Enabled
+INFO - 2025-07-29 17:36:03 --> Utf8 Class Initialized
+INFO - 2025-07-29 17:36:03 --> URI Class Initialized
+INFO - 2025-07-29 17:36:03 --> Router Class Initialized
+INFO - 2025-07-29 17:36:03 --> Output Class Initialized
+INFO - 2025-07-29 17:36:03 --> Security Class Initialized
+DEBUG - 2025-07-29 17:36:03 --> Global POST, GET and COOKIE data sanitized
+INFO - 2025-07-29 17:36:03 --> Input Class Initialized
+INFO - 2025-07-29 17:36:03 --> Language Class Initialized
+INFO - 2025-07-29 17:36:03 --> Loader Class Initialized
+DEBUG - 2025-07-29 17:36:03 --> Config file loaded: /home/hg260vn60h0l/public_html/resto.vermont-technology.com/application/config/config.php
+INFO - 2025-07-29 17:36:03 --> Helper loaded: url_helper
+INFO - 2025-07-29 17:36:03 --> Helper loaded: file_helper
+INFO - 2025-07-29 17:36:03 --> Helper loaded: form_helper
+INFO - 2025-07-29 17:36:03 --> Helper loaded: my_helper
+INFO - 2025-07-29 17:36:03 --> Helper loaded: language_helper
+INFO - 2025-07-29 17:36:03 --> Language file loaded: language/english/english_lang.php
+INFO - 2025-07-29 17:36:03 --> Database Driver Class Initialized
+INFO - 2025-07-29 17:36:03 --> Email Class Initialized
+INFO - 2025-07-29 17:36:03 --> Session: Class initialized using 'database' driver.
+INFO - 2025-07-29 17:36:03 --> Controller Class Initialized
+INFO - 2025-07-29 17:36:03 --> File loaded: /home/hg260vn60h0l/public_html/resto.vermont-technology.com/application/views/waste/REST_API_JSON.php
+INFO - 2025-07-29 17:36:04 --> Config Class Initialized
+INFO - 2025-07-29 17:36:04 --> Hooks Class Initialized
+DEBUG - 2025-07-29 17:36:04 --> UTF-8 Support Enabled
+INFO - 2025-07-29 17:36:04 --> Utf8 Class Initialized
+INFO - 2025-07-29 17:36:04 --> URI Class Initialized
+INFO - 2025-07-29 17:36:04 --> Router Class Initialized
+INFO - 2025-07-29 17:36:04 --> Output Class Initialized
+INFO - 2025-07-29 17:36:04 --> Security Class Initialized
+DEBUG - 2025-07-29 17:36:04 --> Global POST, GET and COOKIE data sanitized
+INFO - 2025-07-29 17:36:04 --> Input Class Initialized
+INFO - 2025-07-29 17:36:04 --> Language Class Initialized
+ERROR - 2025-07-29 17:36:04 --> 404 Page Not Found: Faviconico/index
+ERROR - 2025-07-29 17:36:04 --> Severity: error --> Exception: Call to undefined function base_url() /home/hg260vn60h0l/public_html/resto.vermont-technology.com/application/views/errors/html/error_404.php 8
+INFO - 2025-07-29 17:36:05 --> Config Class Initialized
+INFO - 2025-07-29 17:36:05 --> Hooks Class Initialized
+DEBUG - 2025-07-29 17:36:05 --> UTF-8 Support Enabled
+INFO - 2025-07-29 17:36:05 --> Utf8 Class Initialized
+INFO - 2025-07-29 17:36:05 --> URI Class Initialized
+DEBUG - 2025-07-29 17:36:05 --> No URI present. Default controller set.
+INFO - 2025-07-29 17:36:05 --> Router Class Initialized
+INFO - 2025-07-29 17:36:05 --> Output Class Initialized
+INFO - 2025-07-29 17:36:05 --> Security Class Initialized
+DEBUG - 2025-07-29 17:36:05 --> Global POST, GET and COOKIE data sanitized
+INFO - 2025-07-29 17:36:05 --> Input Class Initialized
+INFO - 2025-07-29 17:36:05 --> Language Class Initialized
+INFO - 2025-07-29 17:36:05 --> Loader Class Initialized
+DEBUG - 2025-07-29 17:36:05 --> Config file loaded: /home/hg260vn60h0l/public_html/resto.vermont-technology.com/application/config/config.php
+INFO - 2025-07-29 17:36:05 --> Helper loaded: url_helper
+INFO - 2025-07-29 17:36:05 --> Helper loaded: file_helper
+INFO - 2025-07-29 17:36:05 --> Helper loaded: form_helper
+INFO - 2025-07-29 17:36:05 --> Helper loaded: my_helper
+INFO - 2025-07-29 17:36:05 --> Helper loaded: language_helper
+INFO - 2025-07-29 17:36:05 --> Language file loaded: language/english/english_lang.php
+INFO - 2025-07-29 17:36:05 --> Database Driver Class Initialized
+INFO - 2025-07-29 17:36:05 --> Email Class Initialized
+INFO - 2025-07-29 17:36:05 --> Session: Class initialized using 'database' driver.
+INFO - 2025-07-29 17:36:05 --> Controller Class Initialized
+INFO - 2025-07-29 17:36:05 --> File loaded: /home/hg260vn60h0l/public_html/resto.vermont-technology.com/application/views/waste/REST_API_JSON.php
+INFO - 2025-07-29 17:36:06 --> Config Class Initialized
+INFO - 2025-07-29 17:36:06 --> Hooks Class Initialized
+DEBUG - 2025-07-29 17:36:06 --> UTF-8 Support Enabled
+INFO - 2025-07-29 17:36:06 --> Utf8 Class Initialized
+INFO - 2025-07-29 17:36:06 --> URI Class Initialized
+DEBUG - 2025-07-29 17:36:06 --> No URI present. Default controller set.
+INFO - 2025-07-29 17:36:06 --> Router Class Initialized
+INFO - 2025-07-29 17:36:06 --> Output Class Initialized
+INFO - 2025-07-29 17:36:06 --> Security Class Initialized
+DEBUG - 2025-07-29 17:36:06 --> Global POST, GET and COOKIE data sanitized
+INFO - 2025-07-29 17:36:06 --> Input Class Initialized
+INFO - 2025-07-29 17:36:06 --> Language Class Initialized
+INFO - 2025-07-29 17:36:06 --> Loader Class Initialized
+DEBUG - 2025-07-29 17:36:06 --> Config file loaded: /home/hg260vn60h0l/public_html/resto.vermont-technology.com/application/config/config.php
+INFO - 2025-07-29 17:36:06 --> Helper loaded: url_helper
+INFO - 2025-07-29 17:36:06 --> Helper loaded: file_helper
+INFO - 2025-07-29 17:36:06 --> Helper loaded: form_helper
+INFO - 2025-07-29 17:36:06 --> Helper loaded: my_helper
+INFO - 2025-07-29 17:36:06 --> Helper loaded: language_helper
+INFO - 2025-07-29 17:36:06 --> Language file loaded: language/english/english_lang.php
+INFO - 2025-07-29 17:36:06 --> Database Driver Class Initialized
+INFO - 2025-07-29 17:36:06 --> Email Class Initialized
+INFO - 2025-07-29 17:36:06 --> Session: Class initialized using 'database' driver.
+INFO - 2025-07-29 17:36:06 --> Controller Class Initialized
+INFO - 2025-07-29 17:36:06 --> File loaded: /home/hg260vn60h0l/public_html/resto.vermont-technology.com/application/views/waste/REST_API_JSON.php
